b4cbebc9bddc5f5e3dbab8d3d8bc1f0af3268db8
Romain Lorrilliere <romainlorrilliere@gmail.com>
2021-04-23 14:22:33 +0200

    Update main_kivikuaka.r

--- main_kivikuaka.r
+++ main_kivikuaka.r
@@ -1,11 +1,10 @@
 source("functions/fun_generic.r")
-source("functions/fun_getData.r")
 source("functions/fun_rmd.r")
 source("functions/fun_importation.r")
 
 
 
-main_exploration_rmd <- function(importing=TRUE,exploration=FALSE,importing_file.rmd="importing_data_update.rmd",importing_file.out="importing_dataGPS",exploration_file.rmd="importing_data_update.rmd",exploration_file.out="importing_dataGPS",rep.out="output_html",format_output="html",file.data="data/data_kivikuaka_envents.csv",user = "romainlorrilliere",pw="xxx",save.fig=FALSE,render.clean=TRUE) {
+main_exploration_rmd <- function(importing=TRUE,exploration=FALSE,importing_file.rmd="importing_data_update.rmd",importing_file.out="importing_dataGPS",exploration_file.rmd="importing_data_update.rmd",exploration_file.out="importing_dataGPS",rep.out="output_md",format_output="html",file.data="data/data_kivikuaka_envents.csv",user = "romainlorrilliere",pw="xxx",save.fig=FALSE,render.clean=FALSE) {
 
     if(importing)
         importing_rmd(file.rmd=importing_file.rmd,file.out=importing_file.out,rep.out=rep.out,format_output=format_output,file.data=,mb_user = user,mb_pw = pw,save.fig=save.fig,render.clean=render.clean)
